13348e893189b5d0b8668f769af1d6d922b8c622
Marc Anthony <marc_gears@hotmail.com>
2022-04-21 12:35:12 -0500

    Se corrigieron errores y se agrego view binding

--- app/src/main/java/com/em_and_ei/company/testaplication/MainActivity.kt
+++ app/src/main/java/com/em_and_ei/company/testaplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.em_and_ei.company.testaplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.em_and_ei.company.testaplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.btnVerifyNumber.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+                .setTitle( if (checkPrimeNumber()) "Es primo" else "No es Primo")
+                .setPositiveButton("Aceptar") { dialogInterface, _ ->
+                    dialogInterface.dismiss()
+                }
+            builder.create().show()
+            checkPrimeNumber()
+        }
+    }
+
+    private fun checkPrimeNumber(): Boolean{
+        val number = binding.itInputNumber.text.toString().toInt()
+        var m = 2
+        var flag = true
+        while (flag  && (m < number)){
+            if(number % m == 0){
+                flag = false
+            }else{
+                m += 1
+            }
+        }
+       return flag
     }
 }
